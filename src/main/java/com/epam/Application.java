97e7b65f6cd920bc11740f814486a89adc9759de
Andriy <andriymattt@gmail.com>
2019-11-21 21:01:27 +0200

    add logger to Application class

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,10 +1,16 @@
 package com.epam;
 
+import org.apache.logging.log4j.*;
 
 public class Application {
-
+    private static Logger logger1 = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-
+        logger1.trace("This is a trace message");
+        logger1.debug("This is a debug message");
+        logger1.info("This is an info message");
+        logger1.warn("This is a warn message");
+        logger1.error("This is an error message");
+        logger1.fatal("This is a fatal message");
     }
 }
