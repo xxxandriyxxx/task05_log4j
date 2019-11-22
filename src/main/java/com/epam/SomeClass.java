485072a9be2b1feb16e1856f49134fdc95b78372
Andriy <andriymattt@gmail.com>
2019-11-22 02:21:40 +0200

    add SomeClass class with its logger to use different loggers

---
+++ src/main/java/com/epam/SomeClass.java
@@ -0,0 +1,19 @@
+package com.epam;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SomeClass {
+
+    private static Logger logger2 = LogManager.getLogger(Application.class);
+
+    public static void GenerateLog(String[] args) {
+        logger2.trace("This is a trace message !!!");
+        logger2.debug("This is a debug message !!!");
+        logger2.info("This is an info message !!!");
+        logger2.warn("This is a warn message !!!");
+        logger2.error("This is an error message !!!");
+        logger2.fatal("This is a fatal message !!!");
+    }
+    
+}
